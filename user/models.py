eb9c040234bb936c921c59ed9b2a27474afc098e
Yahor Tsyplakou <yahor.tsyplakou@gmail.com>
2024-10-30 00:53:13 +0100

    Add courses and balance to User model.

--- user/models.py
+++ user/models.py
@@ -3,9 +3,16 @@ from django.db import models
 
 
 class User(AbstractUser):
+    courses = models.ManyToManyField(
+        'course.Course',
+        related_name='users',
+        help_text='Courses taken by the user',
+        through='user_course.UserCourse',
+    )
     achievements = models.ManyToManyField(
         'course.Achievement',
         related_name='users',
         help_text='Achievements of the user',
         through='user_course.UserAchievement',
     )
+    balance = models.DecimalField(decimal_places=2, max_digits=10, default=0)

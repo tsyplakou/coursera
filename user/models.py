4ace24dfe3c953dfe7a906c71ce52fbe08d091b8
Yahor Tsyplakou <yahor.tsyplakou@gmail.com>
2024-10-29 20:12:44 +0100

    Add m2m from User to course Achievements.

--- user/models.py
+++ user/models.py
@@ -1,5 +1,11 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 
 class User(AbstractUser):
-    pass
+    achievements = models.ManyToManyField(
+        'course.Achievement',
+        related_name='users',
+        help_text='Achievements of the user',
+        through='user_course.UserAchievement',
+    )
--- user_course/models.py
+++ user_course/models.py
@@ -25,3 +25,24 @@ class UserCourse(models.Model):
         unique_together = (
             ('user', 'course'),
         )
+
+
+class UserAchievement(models.Model):
+    archived_date = models.DateTimeField(auto_now_add=True)
+    achievement = models.ForeignKey(
+        'course.Achievement',
+        on_delete=models.PROTECT,
+        related_name='+',
+        help_text='Achievement associated with the user',
+    )
+    user = models.ForeignKey(
+        'user.User',
+        on_delete=models.PROTECT,
+        related_name='+',
+        help_text='User achievements',
+    )
+
+    class Meta:
+        unique_together = (
+            ('user', 'achievement'),
+        )

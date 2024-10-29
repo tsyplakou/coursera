7ea6933c3358c2a6efd5983cf5658bd67d8969e1
Yahor Tsyplakou <yahor.tsyplakou@gmail.com>
2024-10-29 19:41:00 +0100

    Add restriction for purchasing course once per user. Add related names for FK to user and course.

--- user_course/models.py
+++ user_course/models.py
@@ -3,10 +3,25 @@ from django.core.validators import MaxValueValidator
 
 
 class UserCourse(models.Model):
-    user = models.ForeignKey('user.User', on_delete=models.PROTECT)
-    course = models.ForeignKey('course.Course', on_delete=models.PROTECT)
+    user = models.ForeignKey(
+        'user.User',
+        on_delete=models.PROTECT,
+        related_name='user_courses',
+        help_text='User associated with the course',
+    )
+    course = models.ForeignKey(
+        'course.Course',
+        on_delete=models.PROTECT,
+        related_name='user_courses',
+        help_text='Course associated with the user',
+    )
     join_date = models.DateTimeField(auto_now_add=True)
     progress = models.PositiveIntegerField(
         default=0,
         validators=[MaxValueValidator(100)],
     )
+
+    class Meta:
+        unique_together = (
+            ('user', 'course'),
+        )

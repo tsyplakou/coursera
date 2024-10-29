7fb3b2e20d4258282284261927abd3f0f56fa3a9
Yahor Tsyplakou <yahor.tsyplakou@gmail.com>
2024-10-29 18:59:50 +0100

    Add base templates folder as project templates to settings.

--- coursera/settings.py
+++ coursera/settings.py
@@ -63,9 +63,7 @@ ROOT_URLCONF = 'coursera.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [
-            'templates',
-        ],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
